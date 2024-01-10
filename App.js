92b025a326d3f5f275c2b51300e026a90881c721
Acidixzz <chasemccainn@aol.com>
2024-01-10 02:51:29 -0800

    link the doc picker to ios build on someones mac

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import DocumentPicker from 'react-native-document-picker';
 
 export default function App() {
+  
+  const pickMultipleDocuments = async () => {
+    try {
+      const results = await DocumentPicker.pickMultiple({
+        type: [DocumentPicker.types.audio],
+      });
+
+      console.log(results);
+
+      // Handle the selected documents
+    } catch (error) {
+      if (DocumentPicker.isCancel(error)) {
+        console.log('Document picking cancelled');
+      } else {
+        console.error('Error picking documents:', error);
+      }
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity onPress={pickMultipleDocuments} style={{padding: 10, shadowOpacity: .5, shadowOffset: [0,0], backgroundColor: '#dbdbd3'}}>
+        <Text>Pick Multiple Documents</Text>
+      </TouchableOpacity>
     </View>
   );
 }
