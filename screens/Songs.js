e9aafe446e74821c2ba13e180f4fc19237b81bc4
Acidixzz <chasemccainn@aol.com>
2024-05-31 20:57:01 -0700

    CHanged entire backend to sqlite, holy moly I love databases :D

--- screens/Songs.js
+++ screens/Songs.js
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Pressable, FlatList, FlatListComponent, VirtualizedList } from 'react-native';
+import { Alert, StyleSheet, Text, View, TouchableOpacity, Pressable, FlatList, FlatListComponent, VirtualizedList } from 'react-native';
 import * as DocumentPicker from "expo-document-picker";
-import Storage from "../Storage";
 import * as MediaLibrary from "expo-media-library";
 import SongComponent from './components/SongComponent';
-
+import { Ionicons } from '@expo/vector-icons';
+import * as SQLite from 'expo-sqlite';
+import {v5 as uuidv5} from 'uuid';
 
 export default function Songs() {
-
-  const [allSongs,setAllSongs] = useState([]);
-  const DB = new Storage();
-
-  async function getAllSongs() {
-    setAllSongs(await DB.loadSongs());
-  }
-
-  useEffect(() => {
-    getAllSongs();
-  }, []);
+  const db = SQLite.openDatabaseSync('TuneVault.db');
+  const [isLoading, setIsLoading] = useState(true);
+  const [songs, setSongs] = useState([]);
   
+  useEffect(() => {
+    db.withTransactionSync(async () =>{
+      await db.execAsync('CREATE TABLE IF NOT EXISTS songs (SONG_GU TEXT PRIMARY KEY, NAME TEXT NOT NULL, ARTIST TEXT, FILE_PATH TEXT NOT NULL, PICTURE BLOB, DATE_ADDED TEXT NOT NULL)');
+    });
+    
+    db.withTransactionSync(async() => {
+      const SELECT = await db.getAllAsync('SELECT * FROM songs');
+      setSongs(SELECT);
+      //console.log(query);
+
+      
+    });
+
+    setIsLoading(false);
+  }, []);
+
   const pickMultipleSongs = async () => {
     try {
 
@@ -37,12 +46,11 @@ export default function Songs() {
 
       console.log(results);
 
-      await DB.storeSongs(results);
-      await getAllSongs();
-
       // Handle the selected documents
+      await insertRows(results);
+
     } catch (error) {
-      if (results.canceled == true) {
+      if (results && results.canceled) {
         console.log('Document picking cancelled');
       } else {
         console.error('Error picking documents:', error);
@@ -50,50 +58,101 @@ export default function Songs() {
     }
   };
 
-  const clearSongs = async () => {
-    await DB.clearStorage();
-    await getAllSongs();
+  const insertRows = async (files) => {
+    try {
+      const values = [];
+      if (files.assets != null) {
+        let query = 'INSERT INTO songs (SONG_GU, NAME, ARTIST, FILE_PATH, DATE_ADDED) VALUES ';
+        let index = 0
+        for (const file of files.assets){
+          query+= `(${Array(5).fill('?').join(', ')})`;
+          if (index < files.assets.length - 1){
+            query+= ', ';
+          }
+          values.push(...[generateRandomGUID(), file.name, 'Juice WRLD', file.uri, new Date().toISOString()]);
+          index++;
+        }
+
+        // console.log(query);
+        // console.log(values);
+
+        db.withTransactionSync(async() => {
+          console.log(db.runSync(query, values));
+
+          const SELECT = await db.getAllAsync('SELECT * FROM songs');
+          setSongs(SELECT);
+        });
+      }
+    } catch (error) {
+      console.error('Error inserting rows:', error);
+    }
   }
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 100 }}>
+  const deleteRows = async (guid) => {
+    try {
+      db.withTransactionSync(async() => {
+        await db.runAsync('DELETE FROM songs WHERE SONG_GU == ?', guid);
+
+        const SELECT = await db.getAllAsync('SELECT * FROM songs');
+        setSongs(SELECT);
+      });
+    } catch (error) {
       
-      <TouchableOpacity onPress={pickMultipleSongs} style={{padding: 10, shadowOpacity: .5, shadowOffset: [0,0], backgroundColor: '#17b6ff', shadowColor : '#17b6ff'}}>
-        <Text style= {{color : '#ffffff'}}>Pick Multiple Songs</Text> 
-      </TouchableOpacity>
-      <TouchableOpacity onPress={clearSongs} style={{padding: 10, shadowOpacity: .5, shadowOffset: [0,0], backgroundColor: '#c21d1d', shadowColor : '#c21d1d', marginTop: 10}}>
-        <Text style= {{color : '#ffffff'}}>Clear Storage</Text>
-      </TouchableOpacity>
+    }
+  }
 
-      <View style={{ flexDirection: 'row' }}>
-        <TouchableOpacity onPress={DB.AudioHandler.play} style={{padding: 10, shadowOpacity: .5, shadowOffset: [0,0], backgroundColor: '#000000', shadowColor : '#000000', marginTop: 10, marginRight: 10}}>
-          <Text style= {{color : '#ffffff'}}>Play Sound</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={DB.AudioHandler.pause} style={{padding: 10, shadowOpacity: .5, shadowOffset: [0,0], backgroundColor: '#000000', shadowColor : '#000000', marginTop: 10, marginRight: 10}}>
-          <Text style= {{color : '#ffffff'}}>Pause Sound</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={DB.AudioHandler.prev} style={{padding: 10, shadowOpacity: .5, shadowOffset: [0,0], backgroundColor: '#000000', shadowColor : '#000000', marginTop: 10, marginRight: 10}}>
-          <Text style= {{color : '#ffffff'}}>Previous</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={DB.AudioHandler.next} style={{padding: 10, shadowOpacity: .5, shadowOffset: [0,0], backgroundColor: '#000000', shadowColor : '#000000', marginTop: 10, marginRight: 10}}>
-          <Text style= {{color : '#ffffff'}}>Next</Text>
-        </TouchableOpacity>
-        {/* <TouchableOpacity onPress={() => {console.log(DB.AudioHandler.songList)}} style={{padding: 10, shadowOpacity: .5, shadowOffset: [0,0], backgroundColor: '#000000', shadowColor : '#000000', marginTop: 10, marginRight: 10}}>
-          <Text style= {{color : '#ffffff'}}>bruh</Text>
-        </TouchableOpacity> */}
-      </View>  
+  function generateRandomGUID() {
+    // Generate a random hexadecimal string (8 characters)
+    const randomHex = () => Math.floor(Math.random() * 0x10000).toString(16).padStart(4, '0');
+    const guid = `${randomHex()}${randomHex()}-${randomHex()}-${randomHex()}-${randomHex()}-${randomHex()}${randomHex()}${randomHex()}`;
+    return guid.toUpperCase(); // Convert to uppercase for consistency
+  }
 
-      {/* <TouchableOpacity onPress={() => {console.log(DB.AudioHandler.curSong)}} style={{padding: 10, shadowOpacity: .5, shadowOffset: [0,0], backgroundColor: '#000000', shadowColor : '#000000', marginTop: 10, marginRight: 10}}>
-          <Text style= {{color : '#ffffff'}}>song</Text>
-        </TouchableOpacity> */}
+  const settingsPopup = (song) => {
+    Alert.alert(
+      'Confirmation',
+      'Do you want to remove this song from your library?',
+      [
+        {
+          text: 'No',
+          onPress: () => console.log('No Pressed'),
+          style: 'cancel',
+        },
+        { text: 'Yes', onPress: () => deleteRows(song.SONG_GU) },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start', marginTop: 100 }}>
+      
+      <View style={{justifyContent: 'center', alignItems: 'center', marginEnd: 'auto', marginStart: 'auto'}}>
+        <TouchableOpacity onPress={pickMultipleSongs} style={{padding: 10, shadowOpacity: .5, shadowOffset: [0,0], backgroundColor: '#17b6ff', shadowColor : '#17b6ff'}}>
+          <Text style= {{color : '#ffffff'}}>Pick Multiple Songs</Text> 
+        </TouchableOpacity>
+
+        <View style={{ flexDirection: 'row' }}>
+          {/*play, pause, next, prev*/}
+        </View>  
+      </View>
             
-      <FlatList
+      <FlatList style= {{start: '5%', marginTop: 20, width: '90%'}}
         showsVerticalScrollIndicator={true}
-        data={allSongs} 
+        data={songs} 
         renderItem={({item}) => (
-          <SongComponent item={item}/>
+          <View>
+          <View flexDirection='row'>
+            <SongComponent item={item}/>
+            <Pressable onPress={() => {settingsPopup(item)}}>
+              <Ionicons style={{marginTop: 18}} name="ellipsis-horizontal-sharp" size={24} color="black" />
+            </Pressable>
+          </View>
+          <View style= {{height: 1, width: '100%', backgroundColor: '#808080', marginTop: 5}}/>
+          </View>
         )}
-        />
+        /> 
+        {/* this is purely for reference as this syntax can be used to dynamically render what songs are loaded */}
 
     </View>
   );
--- screens/components/SongComponent.js
+++ screens/components/SongComponent.js
@@ -1,11 +1,21 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const SongComponent = ({item}) => {
   //console.log(item);
   return (
-    <Pressable>
-      <Text>{item?.name}</Text>
+    <Pressable width="90%" onPress={() => {console.log(item.NAME)}}>
+      <View style= {{flexDirection: 'row', marginTop: 5}}>
+
+        <View style= {{height: 50, width: 50, backgroundColor: 'black'}}>
+          {/* Thumbnail image goes here */}
+        </View>
+
+        <View style= {{start: "25%", flexDirection: 'column'}}>
+          <Text style= {{fontSize: 20}}>{item?.NAME}</Text>
+          {/* <Text style= {{marginTop: 5}}>Juice WRLD</Text> */}
+        </View>
+      </View>
     </Pressable>
   );
 }
